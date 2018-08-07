875f1bc1e5dcc85ec399619e5adb207c0315dfca
Michele Beltrame <mb@italpro.net>
2018-08-07 23:05:06 +0200

    Iron router works

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
-use iron::status::Status;
+use iron::status;
+use router::Router;
+
+fn home(_req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "We do not have a home page yet!")))
+}
+
+fn shortcode(req: &mut Request) -> IronResult<Response> {
+// get '/:shortcode' => [shortcode => qr/[A-Za-z0-9]{8}/] => sub($self) {
+//     my $link = $self->pg->db->query('select targeturl from links where shortcode = ?', $self->stash->{shortcode})->hash;
+//     if (defined $link) {
+//         $self->res->code(303);
+//         return $self->redirect_to( $link->{targeturl} );
+//     }
+//     return $self->render(text => 'Notfound-shortcode');
+    let ref query = req.extensions.get::<Router>().unwrap().find("shortcode").unwrap_or("X");
+    Ok(Response::with((status::Ok,
+        format!("Redirecting you to URL for shortcode: {} ", *query)
+    )))
+}
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((Status::Ok, "Hello world!")))
-    }).http("localhost:3500");
+    let mut router = Router::new(); 
+    router.get("/", home, "home");
+    router.get("/:shortcode", shortcode, "shortcode");
+
+    Iron::new(router).http("localhost:3000").unwrap();
 }
