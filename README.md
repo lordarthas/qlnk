d952ad80452f423f614ad4aa3dd4aa2586978e8a
Michele Beltrame <mb@italpro.net>
2018-08-08 08:09:00 +0200

    readme + license

---
+++ README.md
@@ -0,0 +1,11 @@
+# qlnk
+
+## URL shortener in Rust
+
+### TODO
+
+* Make the basic work as a drop-in replacement for the Perl equivalent. I'll need to use iron, postgres and json crates
+* Read configuration from a (toml?) files
+
+
+License: MPL-2.0
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! # URL shortener in Rust
+//! 
+//! ## TODO
+//! 
+//! * Make the basic work as a drop-in replacement for the Perl equivalent. I'll need to use iron, postgres and json crates
+//! * Read configuration from a (toml?) files
+//! 
+
 extern crate iron;
 extern crate router;
 
