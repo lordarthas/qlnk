86f547b9e8ba41d38c77c337762d51e8f0474a7b
Michele Beltrame <mb@italpro.net>
2018-08-10 23:11:18 +0200

    Attempting to setup a stash

---
+++ src/lib.rs
@@ -0,0 +1,114 @@
+extern crate actix;
+extern crate actix_web;
+extern crate postgres;
+extern crate r2d2;
+extern crate r2d2_postgres;
+
+#[macro_use] extern crate failure;
+#[macro_use] extern crate lazy_static;
+
+use actix::prelude::*;
+use actix_web::{server, App, http::Method, HttpRequest, HttpResponse};
+use r2d2_postgres::{TlsMode, PostgresConnectionManager};
+use failure::Error;
+
+struct DbConfig {
+    host    : String,
+    name    : String,
+    user    : String,
+    pwd     : String
+}
+
+struct Config {
+    db      : DbConfig,
+    addr    : String,
+    auth    : String
+}
+
+struct Stash {
+    dbp: r2d2::Pool<PostgresConnectionManager>,
+}
+
+pub struct Qlnk {
+    config  : Config
+}
+
+lazy_static! {
+    static ref CONFIG   : Config = readconfig();
+    // "std::cell::RefCell<postgres::InnerConnection>` cannot be shared between threads safely"
+    //static ref DBC      : postgres::Connection = connect_db(); 
+}
+
+pub fn run() {
+    let dbpool = connect_db();
+
+    server::HttpServer::new(move || App::with_state(Stash{ dbp: dbpool.clone() })
+        .resource("/", |r| r.f(home))
+        .resource("/find_shortcode/{shortcode}", |r| r.method(Method::GET).f(find_shortcode))
+        .resource("/create", |r| r.method(Method::POST).f(create_shortcode))
+    ).bind("127.0.0.1:3000").unwrap().run();
+}
+
+fn home(_req: &HttpRequest<Stash>) -> &'static str {
+    "We do not have a home page yet!"
+}
+
+fn find_shortcode(req: HttpRequest<Stash>) -> Result<HttpResponse, Error> {
+// get '/:shortcode' => [shortcode => qr/[A-Za-z0-9]{8}/] => sub($self) {
+//     my $link = $self->pg->db->query('select targeturl from links where shortcode = ?', $self->stash->{shortcode})->hash;
+//     if (defined $link) {
+//         $self->res->code(303);
+//         return $self->redirect_to( $link->{targeturl} );
+//     }
+//     return $self->render(text => 'Notfound-shortcode');
+
+    let shortcode : String = req.match_info().query("shortcode")?;
+    let body = format!("Redirecting you to URL for shortcode: {} ", shortcode);
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain")
+        .body(body))
+}
+
+fn create_shortcode(req: HttpRequest<Stash>) -> Result<HttpResponse, Error> {
+//     let targeturl = String::from("https://www.cattlegrid.info/"); // Static for tests
+
+// // let shortcode: String = rand::thread_rng().gen_ascii_chars().take(len).collect();
+
+//     Ok(Response::with((status::Ok, "Create a shortcode for URL... TODO")))
+
+//     // conn.execute("CREATE TABLE person (
+//     //                 id              SERIAL PRIMARY KEY,
+//     //                 name            VARCHAR NOT NULL,
+//     //                 data            BYTEA
+//     //               )", &[]).unwrap();
+//     // let me = Person {
+//     //     id: 0,
+//     //     name: "Steven".to_string(),
+//     //     data: None,
+//     // };
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain")
+        .body("Creating shortcode"))
+}
+
+/// Reads configuration - it's actually coded inline, but should be read
+/// from an external file
+fn readconfig() -> Config {
+    Config {
+        db : DbConfig {
+            host    : String::from("localhost"),
+            name    : String::from("qlnk"),
+            user    : String::from("qlnk"),
+            pwd     : String::from("pqlnk")
+        },
+        addr: String::from("localhost:3000"),
+        auth: String::from("basicauth"),
+    }
+}
+
+fn connect_db() -> r2d2::Pool<PostgresConnectionManager> {
+    let cstring = format!("postgres://{}:{}@{}/{}", CONFIG.db.user, CONFIG.db.pwd, CONFIG.db.host, CONFIG.db.name);
+    let manager = PostgresConnectionManager::new(cstring, TlsMode::None).unwrap();
+    r2d2::Pool::new(manager).unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -6,70 +6,18 @@
 //! * Read configuration from a (toml?) files
 //! 
 
-extern crate iron;
-extern crate router;
-
-use iron::prelude::*;
-use iron::status;
-use router::Router;
-
-struct DbConfig {
-    host    : String,
-    name    : String,
-    user    : String,
-    pwd     : String
-}
-
-struct Config {
-    db      : DbConfig,
-    addr    : String,
-    auth    : String
-}
-
-fn home(_req: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "We do not have a home page yet!")))
-}
-
-fn find_shortcode(req: &mut Request) -> IronResult<Response> {
-// get '/:shortcode' => [shortcode => qr/[A-Za-z0-9]{8}/] => sub($self) {
-//     my $link = $self->pg->db->query('select targeturl from links where shortcode = ?', $self->stash->{shortcode})->hash;
-//     if (defined $link) {
-//         $self->res->code(303);
-//         return $self->redirect_to( $link->{targeturl} );
-//     }
-//     return $self->render(text => 'Notfound-shortcode');
-    let ref query = req.extensions.get::<Router>().unwrap().find("shortcode").unwrap_or("X");
-    Ok(Response::with((status::Ok,
-        format!("Redirecting you to URL for shortcode: {} ", *query)
-    )))
-}
-
-fn create_shortcode(_req: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Create a shortcode for URL... TODO")))
-}
-
-/// Reads configuration - it's actually coded inline, but should be read
-/// from an external file
-fn readconfig() -> Config {
-    Config {
-        db : DbConfig {
-            host    : String::from("localhost"),
-            name    : String::from("qlnk"),
-            user    : String::from("uqlnk"),
-            pwd     : String::from("pqlnk")
-        },
-        addr: String::from("localhost:3000"),
-        auth: String::from("basicauth"),
-    }
-}
+extern crate qlnk;
 
 fn main() {
-    let config = readconfig();
+    qlnk::run();
+    // let config = readconfig();
 
-    let mut router = Router::new(); 
-    router.get("/", home, "home");
-    router.get("/:shortcode", find_shortcode, "find_shortcode");
-    router.get("/create", create_shortcode, "create_shortcode");
+    // let dbc = connect_db(&config);
 
-    Iron::new(router).http(config.addr).unwrap();
+    // let mut router = Router::new(); 
+    // router.get("/", home, "home");
+    // router.get("/:shortcode", find_shortcode, "find_shortcode");
+    // router.get("/create", create_shortcode, "create_shortcode");
+
+    // Iron::new(router).http(config.addr).unwrap();
 }
